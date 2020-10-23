0fd36864b9c1509b7917fc4e9cd3cfc707965162
Mourad Bougarne <contact@mbougarne.me>
2020-10-23 19:44:43 +0100

    Add content of the posts, user, and start working on loading skeleton

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
 const Article = () => {
+
+    let [posts, setPosts] = useState(null)
+
+    useEffect(() => {
+        setTimeout( async () => {
+            let res = await fetch('https://jsonplaceholder.typicode.com/posts')
+            let data = await res.json()
+            setPosts(data.slice(0, 15))
+
+        }, 5000)
+    })
+
     return (
         <div className="article-container">
             <h2>Articles</h2>
+
+            {posts && posts.map(post => (
+                <article className="article" key={post.id}>
+                    <h4>{post.title}</h4>
+                    <p>{post.body}</p>
+                </article>
+            ))}
+
+            {!posts && <div>Loading...</div>}
         </div>
     )
 }
